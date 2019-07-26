67b7ff7ae25d3161b1a9917a247aa396b53d40ee
smirnovdv <volmondo@gmail.com>
2019-07-26 12:54:39 -0500

    Heroku deployment

---
+++ app.js
@@ -0,0 +1,38 @@
+// // Client ID
+// B4tBs5lRvfBC4Q7jmtKOxg
+
+// API Key
+// PPTr_crOO4P-W4rx7jJXTQsU3kc6pJpJugCbo8yfwMh2jnUOzhXambkkTjYuNOkzAJjGd1v56DPXZ0b_3KumUO7u3k_EcaZ-bBjacfmm-DFlNfkMsY2OP4FoRw85XXYx
+
+'use strict';
+
+const express = require("express")
+const app = express();
+const yelp = require('yelp-fusion');
+const client = yelp.client('PPTr_crOO4P-W4rx7jJXTQsU3kc6pJpJugCbo8yfwMh2jnUOzhXambkkTjYuNOkzAJjGd1v56DPXZ0b_3KumUO7u3k_EcaZ-bBjacfmm-DFlNfkMsY2OP4FoRw85XXYx');
+ 
+
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+})
+
+app.get("/results", (req, res) => {
+    
+    client.search({
+        location: req.query.search,
+        radius: 5000,
+        limit:10,
+        sort_by:"distance"
+      }).then(response => {
+        console.log(response.jsonBody);
+        res.render("results.ejs",{data:response.jsonBody.businesses});
+      }).catch(e => {
+        console.log(e);
+      });
+    
+})
+
+app.use(express.static('public'))
+
+app.listen(3000, () => {console.log("Server running")})
